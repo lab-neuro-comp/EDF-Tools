6c34a0a63aab92a3965c099e38781b38c5c25860
Cristiano Silva Jr <cristianoalvesjr@gmail.com>
2016-12-07 11:13:46 -0200

    Converting spaces to tabs

--- app/sample/sample.go
+++ app/sample/sample.go
@@ -8,46 +8,46 @@ import "github.com/ishiikurisu/edf"
 
 // Processes a folder containing many EDF files
 func main() {
-    var wg sync.WaitGroup
-    var source string
+		var wg sync.WaitGroup
+		var source string
 
-    // Deciding source folder
-    if len(os.Args) > 1 {
-        source = os.Args[1]
-    } else {
-        source = "."
-    }
-    source += "/"
+		// Deciding source folder
+		if len(os.Args) > 1 {
+				source = os.Args[1]
+		} else {
+				source = "."
+		}
+		source += "/"
 
-    // Processing each file on directory
-    files, _ := ioutil.ReadDir(source)
-    for _, file := range files {
-        fileName := file.Name()
-        if validFile(fileName) {
-            wg.Add(1)
-            go process(source + fileName, &wg)
-        }
-    }
-    wg.Wait()
+		// Processing each file on directory
+		files, _ := ioutil.ReadDir(source)
+		for _, file := range files {
+				fileName := file.Name()
+				if validFile(fileName) {
+						wg.Add(1)
+						go process(source + fileName, &wg)
+				}
+		}
+		wg.Wait()
 }
 
 // Checks if the file name is validFile
 func validFile(inlet string) bool {
-  	isEdf := true
-  	ext := ".edf"
+		isEdf := true
+		ext := ".edf"
 
-  	for i := 1; i <= 4 && isEdf; i++ {
-    		if inlet[len(inlet)-i] != ext[len(ext)-i] {
-    			   isEdf = false
-    		}
-  	}
+		for i := 1; i <= 4 && isEdf; i++ {
+				if inlet[len(inlet)-i] != ext[len(ext)-i] {
+						 isEdf = false
+				}
+		}
 
-  	return isEdf
+		return isEdf
 }
 
 // Processes an EDF file, extracting the subject name and the sampling rate
 func process(inlet string, wg *sync.WaitGroup) {
-    edfHeader, _ := edf.ReadFile(inlet)
-    fmt.Printf("%s\t%s\t%s\n", inlet, edfHeader["patient"], edf.GetSampling(edfHeader))
-    wg.Done()
+		edfHeader, _ := edf.ReadFile(inlet)
+		fmt.Printf("%s\t%s\t%s\n", inlet, edfHeader["patient"], edf.GetSampling(edfHeader))
+		wg.Done()
 }
